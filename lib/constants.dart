92b775931a92ebf3a1d03dd290bb8248fea0f669
HuuTrong <0306191183@caothang.edu.vn>
2022-01-06 10:08:11 +0700

    modify constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,4 @@ const kTextLightColor = Color(0xFFACACAC);
 const kBackgroundColor = Color.fromRGBO(175, 245, 238,0.8);
 const kDefaultPaddin = 20.0;
 const kDetailColor = Colors.lightGreen;
+const kButtonColor = Color.fromRGBO(145 , 255 ,238 , 0.5); 
